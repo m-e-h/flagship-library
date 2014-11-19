ee78383dd5f6cb43e7480594a534d0059ff215e3
robneu <rob@wpsitecare.com>
2014-11-18 19:52:48 -0600

    Re-structured the main library class

--- flagship-library.php
+++ flagship-library.php
@@ -4,7 +4,7 @@
  *
  * @package     FlagshipLibrary
  * @subpackage  HybridCore
- * @copyright   Copyright (c) 2014, Flagship, LLC
+ * @copyright   Copyright (c) 2014, Flagship Software, LLC
  * @license     GPL-2.0+
  * @link        http://flagshipwp.com/
  * @since       1.0.0
@@ -25,9 +25,17 @@ if ( ! class_exists( 'Flagship_Library' ) ) {
 		 * Used to prefix filters to make them unique.
 		 *
 		 * @since 1.0.0
-		 * @type string
+		 * @type  string
 		 */
 		protected $prefix;
+
+		/**
+		 * Static placeholder for our main class instance.
+		 *
+		 * @since 1.0.0
+		 * @var   Flagship_Library
+		 */
+		private static $instance;
 
 		/**
 		 * Constructor method to initialize the class.
@@ -35,7 +43,7 @@ if ( ! class_exists( 'Flagship_Library' ) ) {
 		 * @since 1.0.0
 		 *
 		 * @param array $args {
-		 *    Configuration options. Optional
+		 *     Configuration options. Optional
 		 *
 		 *    @type string $prefix  Optional. Theme prefix. Defaults to the template name.
 		 *    @type array  $strings List of internationalized strings.
@@ -43,13 +51,60 @@ if ( ! class_exists( 'Flagship_Library' ) ) {
 		 */
 		public function __construct( $args = array() ) {
 			$this->prefix = empty( $args['prefix'] ) ? get_template() : sanitize_key( $args['prefix'] );
-			self::includes();
+		}
+
+		/**
+		 * Throw error on object clone
+		 *
+		 * The whole idea of the singleton design pattern is that there is a single
+		 * object therefore, we don't want the object to be cloned.
+		 *
+		 * @since  1.0.0
+		 * @access protected
+		 * @return void
+		 */
+		public function __clone() {
+			// Cloning instances of the class is forbidden
+			_doing_it_wrong( __FUNCTION__, __( 'Cheatin&#8217; huh?', 'affiliate-wp' ), '1.0' );
+		}
+
+		/**
+		 * Disable unserializing of the class
+		 *
+		 * @since  1.0.0
+		 * @access protected
+		 * @return void
+		 */
+		public function __wakeup() {
+			// Unserializing instances of the class is forbidden
+			_doing_it_wrong( __FUNCTION__, __( 'Cheatin&#8217; huh?', 'affiliate-wp' ), '1.0' );
+		}
+
+		/**
+		 * Main Flagship_Library Instance
+		 *
+		 * Insures that only one instance of Flagship_Library exists in memory at any one
+		 * time. Also prevents needing to define globals all over the place.
+		 *
+		 * @since 1.0.0
+		 * @static
+		 * @uses   Flagship_Library::includes() Include the required files
+		 * @return Flagship_Library
+		 */
+		public static function instance() {
+			if ( ! isset( self::$instance ) && ! ( self::$instance instanceof Flagship_Library ) ) {
+				self::$instance = new Flagship_Library;
+				self::$instance->includes();
+				self::$instance->extensions_includes();
+			}
+			return self::$instance;
 		}
 
 		/**
 		 * Whether the current request is a Customizer preview.
 		 *
 		 * @since   1.0.0
+		 * @access  public
 		 * @return  bool
 		 */
 		public function is_customizer_preview() {
@@ -61,6 +116,7 @@ if ( ! class_exists( 'Flagship_Library' ) ) {
 		 * Whether the current environment is WordPress.com.
 		 *
 		 * @since   1.0.0
+		 * @access  public
 		 * @return  bool
 		 */
 		public function is_wpcom() {
@@ -71,6 +127,7 @@ if ( ! class_exists( 'Flagship_Library' ) ) {
 		 * Return the correct path to the flagship library directory.
 		 *
 		 * @since   1.0.0
+		 * @access  public
 		 * @return  string
 		 */
 		public function get_library_directory() {
@@ -78,17 +135,65 @@ if ( ! class_exists( 'Flagship_Library' ) ) {
 		}
 
 		/**
-		 * Whether the current environment is WordPress.com.
+		 * Return the correct path to the flagship library directory.
+		 *
+		 * @since   1.1.0
+		 * @access  public
+		 * @return  string
+		 */
+		public function get_library_uri() {
+			return apply_filters( 'flagship_library_uri', str_replace( realpath( get_theme_root() ), get_theme_root_uri(), realpath( $this->get_library_directory() ) ) );
+		}
+
+		/**
+		 * Include required library files.
 		 *
 		 * @since   1.0.0
-		 * @return  bool
+		 * @access  private
+		 * @return  void
 		 */
 		public function includes() {
-			$library_dir = $this->get_library_directory();
-			require_once $library_dir . '/classes/class-search-form.php';
-			require_once $library_dir . '/functions/attributes.php';
-			require_once $library_dir . '/functions/hybrid-tweaks.php';
-			require_once $library_dir . '/functions/template-helpers.php';
+			$library_dir = trailingslashit( $this->get_library_directory() );
+			require_once $library_dir . 'classes/class-search-form.php';
+			require_once $library_dir . 'functions/attr.php';
+			require_once $library_dir . 'functions/seo.php';
+			require_once $library_dir . 'functions/template-entry.php';
+			require_once $library_dir . 'functions/template-general.php';
+			require_once $library_dir . 'functions/template.php';
 		}
+
+		/**
+		 * Include extensions init files only when theme support has been added.
+		 *
+		 * @since   1.0.0
+		 * @access  private
+		 * @return  void
+		 */
+		public function extensions_includes() {
+			$extensions_dir = trailingslashit( $this->get_library_directory() ) . 'extensions/';
+			require_if_theme_supports( 'site-logo', $extensions_dir . 'site-logo/init.php' );
+			require_if_theme_supports( 'breadcrumb-trail', $extensions_dir . 'breadcrumb-display/init.php' );
+			require_if_theme_supports( 'flagship-footer-widgets', $extensions_dir . 'footer-widgets/init.php' );
+		}
+
 	}
 }
+
+if ( ! function_exists( 'flagship_library' ) ) {
+	/**
+	 * Grab an instance of the main library class. If you need to reference a
+	 * method in the class for some reason, do it using this function.
+	 *
+	 * Example:
+	 *
+	 * <?php flagship_library()->is_customizer_preview(); ?>
+	 *
+	 * @version 1.0.0
+	 */
+	function flagship_library() {
+		return Flagship_Library::instance();
+	}
+}
+
+// Get the library up and running.
+flagship_library();
