f209e9fbcc90ff1c0d02eb688870feda95d87d3b
robneu <rob@wpsitecare.com>
2015-03-08 00:39:08 -0600

    Split admin and display
    Profiling revealed this to be slightly more performant on the front
    end, plus it’s probably better practice in general. Also cleaned up
    front-end checking and method names.

--- extensions/author-box/classes/author-box.php
+++ extensions/author-box/classes/author-box.php
@@ -40,17 +40,9 @@ class Flagship_Author_Box {
 	 * @return void
 	 */
 	private function wp_hooks() {
-		add_filter( 'hybrid_attr_author-box',   array( $this, 'attr_author_box' ), 10, 2 );
-		add_action( 'tha_entry_after',          array( $this, 'author_box_single' ) );
-		add_action( 'tha_content_top',          array( $this, 'author_box_archive' ) );
-		if ( ! is_admin() ) {
-			return;
-		}
-		add_filter( 'user_contactmethods',      array( $this, 'user_contactmethods' ) );
-		add_action( 'show_user_profile',        array( $this, 'user_fields' ) );
-		add_action( 'edit_user_profile',        array( $this, 'user_fields' ) );
-		add_action( 'personal_options_update',  array( $this, 'meta_save' ) );
-		add_action( 'edit_user_profile_update', array( $this, 'meta_save' ) );
+		add_filter( 'hybrid_attr_author-box', array( $this, 'attr_author_box' ), 10, 2 );
+		add_action( 'tha_entry_after',        array( $this, 'single' ) );
+		add_action( 'tha_content_top',        array( $this, 'archive' ) );
 	}
 
 	/**
@@ -87,7 +79,7 @@ class Flagship_Author_Box {
 	 * @uses   locate_template() Load the single author box template.
 	 * @return void
 	 */
-	public function author_box_single() {
+	public function single() {
 		if ( ! is_singular( apply_filters( 'flagship_author_box_types', array( 'post' ) ) ) ) {
 			return;
 		}
@@ -95,7 +87,7 @@ class Flagship_Author_Box {
 		$display = get_the_author_meta( 'flagship_author_box_single' );
 
 		// Bail if display is disabled. Continue if no author meta exists.
-		if ( '' !== $display && 0 === absint( $display ) ) {
+		if ( '' !== $display && '0' === "$display" ) {
 			return;
 		}
 
@@ -114,16 +106,17 @@ class Flagship_Author_Box {
 	 * @uses   locate_template() Load the archive author box template.
 	 * @return void
 	 */
-	public function author_box_archive() {
+	public function archive() {
 		if ( ! is_author() || is_paged() ) {
 			return;
 		}
 		$display = get_the_author_meta( 'flagship_author_box_archive' );
 
 		// Bail if display is disabled or no author meta exists.
-		if ( '' === $display || '0' === $display ) {
+		if ( empty( $display ) ) {
 			return;
 		}
+
 		$types = apply_filters( 'flagship_author_box_types', array( 'post' ) );
 
 		if ( ! in_array( get_post_type(), (array) $types ) ) {
@@ -135,74 +128,6 @@ class Flagship_Author_Box {
 			return require_once locate_template( 'flagship/author-box-archive.php' );
 		}
 		require_once $this->lib_dir . '/templates/author-box-archive.php';
-	}
-
-	/**
-	 * Add additional contact methods for registered users.
-	 *
-	 * @since  1.4.0
-	 * @access public
-	 * @param  array $contactmethods Existing contact methods.
-	 * @return array $contactmethods Modifed contact methods.
-	 */
-	public function user_contactmethods( array $contactmethods ) {
-		$contactmethods['googleplus'] = __( 'Google+', 'flagship-library' );
-		$contactmethods['twitter']    = __( 'Twitter (Without @)', 'flagship-library' );
-		$contactmethods['facebook']   = __( 'Facebook', 'flagship-library' );
-		return $contactmethods;
-	}
-
-	/**
-	 * Add fields for author archives contents to the user edit screen.
-	 *
-	 * @since  1.4.0
-	 * @access public
-	 * @param  $user Object WordPress user object.
-	 * @return void
-	 */
-	public function user_fields( $user ) {
-		if ( ! current_user_can( 'edit_users', $user->ID ) ) {
-			return false;
-		}
-		$single_box  = get_the_author_meta( 'flagship_author_box_single',  $user->ID );
-		$archive_box = get_the_author_meta( 'flagship_author_box_archive', $user->ID );
-		// Set the single author box to enabled when no author meta has been set.
-		if ( '' === $single_box ) {
-			$single_box = 1;
-		}
-		require_once $this->lib_dir . '/templates/admin/settings-author-box.php';
-	}
-
-	/**
-	 * Update user meta when user edit page is saved.
-	 *
-	 * @since  1.4.0
-	 * @access public
-	 * @param  $user_id integer The current user ID
-	 * @return void
-	 */
-	public function meta_save( $user_id ) {
-		if ( ! current_user_can( 'edit_users', $user_id ) ) {
-			return;
-		}
-
-		$defaults = array(
-			'flagship_author_box_single'  => 0,
-			'flagship_author_box_archive' => 0,
-		);
-
-		if ( ! isset( $_POST['flagbox'] ) || ! is_array( $_POST['flagbox'] ) ) {
-			foreach ( $defaults as $key => $value ) {
-				update_user_meta( $user_id, $key, $value );
-			}
-			return;
-		}
-
-		$meta = wp_parse_args( $_POST['flagbox'], $defaults );
-
-		foreach ( $meta as $key => $value ) {
-			update_user_meta( $user_id, sanitize_key( $key ), absint( $value ) );
-		}
 	}
 
 }
